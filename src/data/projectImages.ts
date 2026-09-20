5bacdc1a4cb0a184a03e6c9baa449fe9cc3d3b35
osama241989 <osama.gr84u@gmail.com>
2026-09-21 01:15:57 +0300

    Update projectImages.ts

--- src/data/projectImages.ts
+++ src/data/projectImages.ts
@@ -1,11 +1,7 @@
 import realestate from "@/assets/p-realestate.jpg.asset.json";
-import mattress from "@/assets/p-mattress.jpg.asset.json";
-import fnb from "@/assets/p-fnb.jpg.asset.json";
-import aceco from "@/assets/p-aceco.jpg.asset.json";
 import berain from "@/assets/p-berain.jpg.asset.json";
 import daffah from "@/assets/p-daffah.jpg.asset.json";
 import carwash from "@/assets/p-carwash.jpg.asset.json";
-import video from "@/assets/p-video.jpg.asset.json";
 import gfsa from "@/assets/p-gfsa.jpg.asset.json";
 import itqan from "@/assets/p-itqan.jpg.asset.json";
 import menu from "@/assets/p-menu.jpg.asset.json";
@@ -15,13 +11,13 @@ import modasafi from "@/assets/p-modasafi.jpg.asset.json";
 
 export const projectImages: Record<string, string> = {
   realestate: realestate.url,
-  mattress: mattress.url,
-  fnb: fnb.url,
-  aceco: aceco.url,
+  mattress: "/project-mattress.jpg",
+  fnb: "/project-fnb.jpg",
+  aceco: "/project-aceco.jpg",
   berain: berain.url,
   daffah: daffah.url,
   carwash: carwash.url,
-  video: video.url,
+  video: "/p-video.jpg",
   gfsa: gfsa.url,
   itqan: itqan.url,
   menu: menu.url,
